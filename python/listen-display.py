71d2105eefd36b1fcdb019f6d7d0ee3c3d6d1780
dkijc <wwjdat2000@yahoo.com>
2018-05-02 17:02:22 -0400

    everything is working as expected

--- python/listen-display.py
+++ python/listen-display.py
@@ -69,20 +69,22 @@ draw = ImageDraw.Draw(image)
 draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
 
 # Draw some shapes.
-draw.ellipse((2,2,22,22), outline=0, fill=255)
-draw.rectangle((24,2,44,22), outline=0, fill=255)
-draw.polygon([(46,22), (56,2), (66,22)], outline=0, fill=255)
-draw.line((68,22,81,2), fill=0)
-draw.line((68,2,81,22), fill=0)
+draw.ellipse((2,2,20,20), outline=0, fill=255)
+#draw.ellipse((8,2,15,1), outline=0, fill=0)
+draw.rectangle((8,9,27,13), outline=0, fill=0)
+# draw.polygon([(46,22), (56,2), (66,22)], outline=0, fill=255)
+# draw.line((68,22,81,2), fill=0)
+# draw.line((68,2,81,22), fill=0)
 
 # Load default font.
 font = ImageFont.load_default()
 
 # Alternatively load a TTF font.
 # Some nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('Minecraftia.ttf', 8)
-
-#draw.text((8,30), 'something is written', font=font)
+# font = ImageFont.truetype('Minecraftia.ttf', 12)
+ # font = ImageFont.truetype('Minecraftia.ttf', 12)
+draw.text((30,0), 'Smart', font=font)
+draw.text((30,10), 'Handlers', font=font)
 # Display image.
 disp.image(image)
 disp.display()
@@ -92,27 +94,27 @@ print('Press Ctrl-C to quit.')
 #ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 
 while True:
-    if draw is not None:
-        del draw
+    # if draw is not None:
+    #      del draw
 
-#    time.sleep(1.0)
+    #time.sleep(1.0)
 
     # Create blank image for drawing.
     # Make sure to create image with mode '1' for 1-bit color.
-    #image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+    # Initialize library.
 
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
-    
-    # Load default font.
-    #font = ImageFont.load_default()
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    #draw = ImageDraw.Draw(image)
+    #font = ImageFont.load_default()
     #todos = json.load(open('../node-todo/todo-list.json'))
 
-    print(data)
+    print(data, "something has received")
     #print "Received Message:", data
     #draw.text((8,30), ser.readline(), font=font)
     #draw.text((8,30), data['table'][0]["description"], font=font)
-    draw.text((8,30), data, font=font)
+    # draw.rectangle((LCD.LCDWIDTH,20,20,30), outline=0, fill=255)
+    draw.rectangle((0,21,LCD.LCDWIDTH-1,43), outline=0, fill=255)
+    draw.multiline_text((2,21), data, font=font)
     disp.image(image)
     disp.display()
+    
